c9d4834c2f542fb423450513928645194cffbe83
lukebrogee <113709371+lukebrogee@users.noreply.github.com>
2025-07-24 17:09:29 -0400

    Finished submit page, started edit page

---
+++ app/Components/EditPage.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+interface CustomFileUploadProps {
+  width?: string;
+  height?: string;
+}
+
+const EditPage: React.FC<CustomFileUploadProps> = ({ width, height }) => {
+
+
+    return (
+        <>
+            <form>
+                <h1>Revise Events As Needed</h1>
+                
+            </form>
+        </>
+
+    );
+}
+
+
+
+
+export default EditPage;
